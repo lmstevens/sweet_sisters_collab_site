01774e616859a81e14d02bc5156fca1b900f4b4e
tuskaneenie <73411319+salenastamp@users.noreply.github.com>
2020-12-07 16:32:01 -0500

    about html, css, and models

--- src/main/java/com/tts/eCommerceService/UserService.java
+++ src/main/java/com/tts/eCommerceService/UserService.java
@@ -6,9 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.tts.eCommerceModel.DetailsUser;
 import com.tts.eCommerceModel.Product;
 import com.tts.eCommerceModel.User;
 import com.tts.eCommerceRepository.UserRepository;
@@ -43,10 +45,10 @@ public class UserService implements UserDetailsService {
         saveExisting(user);
     }
 
-    @Override
+    @Override	
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = findByUsername(username);
         if(user == null) throw new UsernameNotFoundException("Username not found.");
-        return user;
+        return new DetailsUser(user);
     }
 }
